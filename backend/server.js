5285ee0c05cf933947520bdaa43f1418b634ad2d
Divyansh <divdub697@gmail.com>
2025-10-04 22:56:30 +0530

    c7

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,11 @@ app.use("/api/products",productRoutes);
 if (process.env.NODE_ENV === "production") {
    app.use(express.static(path.join(__dirname, "frontend/dist")));
  
-   app.get("/*", (req, res) => {
+   // Catch-all route for SPA
+   app.get(/.*/, (req, res) => {
      res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
    });
  }
- 
 
 app.listen(PORT,()=>{
    console.log(`Server is started on http://localhost:${PORT}`);
