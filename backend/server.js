6ec7d971a32a88b16d941e4229644735b91324d0
Divyansh <divdub697@gmail.com>
2025-10-04 18:27:00 +0530

    c5

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/api/products",productRoutes);
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"/frontend/dist")));
     
-    app.get("/:path(*)",(req,res)=>{
+    app.get("*",(req,res)=>{
       res.sendFile(path.resolve(__dirname,"./frontend/dist/index.html"));
     });
 }
